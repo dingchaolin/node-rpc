108342d0754fa4f8ece9085dc995df308a91c3c6
dingchaolin <18810042351@163.com>
2017-11-07 22:42:13 +0800

    add log

--- redis/subscribe.js
+++ redis/subscribe.js
@@ -2,6 +2,9 @@ let Redis = require('ioredis');
 let redis = new Redis();
 let pub = new Redis();
 let channels = require("./config").getConfig(3,0);
+/*
+每个订阅者的推送通道必须是唯一的，否则无法根据推送通道推送回执信息
+ */
 redis.subscribe(channels, function (err, count) {
     pub.publish(channels[0], "hello word");//client将member发布到news这个频道
 });
